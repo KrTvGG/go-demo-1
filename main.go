e6dbaf85899d4916f8a55abe0b3bf1b0fb7db4a8
KrTv <krtviv@mail.ru>
2026-02-01 17:11:12 +0300

    3 - 8 Ввод и вывод (отклонился и сделал ввод функцией)

--- main.go
+++ main.go
@@ -8,7 +8,18 @@ import (
 const IMTPower = 2
 
 func main() {
-	var userHeight, userKg float64 = 1.8, 100
-	IMT := float64(userKg) / math.Pow(userHeight, IMTPower)
+	fmt.Print("__ Калькулятор индекса массы тела __\n")
+	var userHeight, userKg float64
+	userHeight = takePrompt("Укажите свой рост в метрах")
+	userKg = takePrompt("Укажите свой вес в кг")
+	IMT := userKg / math.Pow(userHeight, IMTPower)
+	fmt.Print("Ваш индекс массы тела: ")
 	fmt.Println(IMT)
+}
+
+func takePrompt(text string) float64 {
+	fmt.Print(text + ": ")
+	var res float64
+	fmt.Scan(&res)
+	return res
 }
